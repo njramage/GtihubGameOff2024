a3493f2370cfd032908436cdea868a56079791b9
LeLionDev <140218623+LeLionDev@users.noreply.github.com>
2024-11-28 19:05:52 +1000

    Update UIManager.cs to remove unused methods.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -184,17 +184,7 @@ public class UIManager : MonoBehaviour
         suspectImages[4] = suspectImage5;
         suspectImages[5] = suspectImage6;
     }
-    public static void RemoveAt<T>(ref T[] arr, int index)
-    {
-        for (int a = index; a < arr.Length - 1; a++)
-        {
-            // moving elements downwards, to fill the gap at [index]
-            arr[a] = arr[a + 1];
-        }
-        // finally, let's decrement Array's size by one
-        Array.Resize(ref arr, arr.Length - 1);
-    }
-
+    
     private int[] uniqueNumbers = {0,1,2,3,4,5};
     private int retry = 9;
     private int getUniqueRandomNumbers() {
